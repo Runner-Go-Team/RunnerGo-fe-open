00f0e5ebab4ccbc7299f2ee011f7dd7f72cab30f
liuguanglei <liuguanglei@apipost.cn>
2023-02-28 11:37:44 +0800

    feat: delete some file

--- config/oss.js
+++ config/oss.js
@@ -1,8 +1,8 @@
 export const OSS_Config = {
-    region: 'oss-cn-beijing',
-    accessKeyId: 'LTAI5tEAzFMCX559VD8mRDoZ',
-    accessKeySecret: '5IV7ZpVx95vBHZ3Y74jr9amaMtXpCQ',
-    bucket: 'apipost',
+    region: '',
+    accessKeyId: '',
+    accessKeySecret: '',
+    bucket: '',
 }
 
 export default {
